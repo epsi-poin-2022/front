47ee6fe36b4415a51b86eecfb118131612f8d799
Loris Cocco <loris.cocco98@gmail.com>
2022-06-21 18:52:59 +0200

    apiurl

--- src/utils/RequestAPI.js
+++ src/utils/RequestAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const APIURL = "http://localhost/api/";
+// export const APIURL = "http://localhost/api/";
+export const APIURL = "https://api-poin-2223.duclos.xyz/api/";
 
 export default function RequestAPI(method = "GET", url = "/", data = []) {
   return axios({
